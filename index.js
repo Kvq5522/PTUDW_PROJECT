df95cf845c5d47addaa2ae9336b3565963b49662
Kvq5522 <89141917+Kvq5522@users.noreply.github.com>
2022-11-26 14:03:37 +0700

    feat: add signin and signup page

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
 const port = 3000;
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static('public'));
+app.use(session({
+    secret: "79bcca27f38f32e3aa043f416e78fcf0",
+    resave: false,
+    saveUninitialized: false
+}));
 app.set('view engine', 'ejs');
-
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/', (req, res) => {
     res.render('./index');
@@ -20,6 +29,20 @@ app.get('/shop', (req, res) => {
 
 app.get('/product/:productID', (req, res) => {
     res.render('./product');
+});
+
+app.get('/user', (req, res) => {
+   if (!req.isAuthenticated()) {
+        res.redirect('/signin');
+   }
+});
+
+app.get('/signin', (req, res) => {
+    res.render('./signin');
+});
+
+app.get('/signup', (req, res) => {
+    res.render('./signup');
 });
 
 app.get('/cart', (req, res) => {
