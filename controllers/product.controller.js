c90262f78c29b3e3930d15f198ad893d14a55f59
Kvq5522 <89141917+Kvq5522@users.noreply.github.com>
2022-12-06 21:57:32 +0700

    feat: add filter to product page

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,23 +2,18 @@ const products = require('../models/Product.model');
 const qs = require('qs');
 
 const getProductPage = (req, res) => {
-    const {sort, color, ...withoutFilter} = req.query;
+    const {sort, color, priceRange, ...withoutFilter} = req.query;
 
-    let name = withoutFilter.name ? withoutFilter.name : '';
-    name = name.charAt(0).toUpperCase() + name.slice(1);
+    let nameFilter = withoutFilter.name ? withoutFilter.name : '';
+    nameFilter = nameFilter.charAt(0).toUpperCase() + nameFilter.slice(1);
+    let colorFilter = color ? color : '';
+    let sortFilter = sort ? sort : 'name';
+    let lowPivot = priceRange ? Number(priceRange.split('-')[0]) : 0;
+    let highPivot = priceRange ? Number(priceRange.split('-')[1]) : 1e9;
+    
 
-    // products.Product.find({name: {'$regex': name}}, (err, data) => {
-    //     if (err) {
-    //         console.log(err);
-    //         res.status(500).send('Internal Server Error');
-    //         return;
-    //     }
-
-    //     console.log(withoutFilter);
-
-    //     res.render('product', { productList: data, originalUrl: `${req.baseUrl}?${qs.stringify(withoutFilter)}` });
-    // });
-    products.Product.find({name: {'$regex': name}}).sort({price: 1}).exec((err, data) => {
+    products.Product.find({name: {'$regex': nameFilter, '$options': 'i'}, color: {'$regex': colorFilter}, 
+    price: {$gte: lowPivot, $lte: highPivot}}).sort(`${sortFilter}`).exec((err, data) => {
         if (err) {
             console.log(err);
             res.status(500).send('Internal Server Error');
